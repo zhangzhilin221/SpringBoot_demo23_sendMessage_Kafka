8df657ca92db7cb201384a9bdc8bfd68639cc300
alin <1714773918@qq.com>
2023-08-12 11:46:33 +0800

    master  commit!

--- src/test/java/com/example/TestGit.java
+++ src/test/java/com/example/TestGit.java
@@ -3,5 +3,8 @@ package com.example;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
+        System.out.println("hot-fix");
+        System.out.println("hot-fix new");
     }
 }
