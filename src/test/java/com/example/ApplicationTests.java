cb51846bb93eaf49e05c31c483e1a80435d04bef
alin <1714773918@qq.com>
2023-08-12 11:50:12 +0800

    hot fix  commit2!

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -8,6 +8,9 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("hot-fix");
+        System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 
 }
