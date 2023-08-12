086f3d1b4ce40d6a5b802c0348e9b83c72a3616b
alin <1714773918@qq.com>
2023-08-12 11:15:54 +0800

    idea second commit!

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testGit() {
+        System.out.println("idea second commit!");
     }
 
 }
