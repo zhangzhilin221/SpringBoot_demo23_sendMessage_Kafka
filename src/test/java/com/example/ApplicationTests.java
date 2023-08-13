fa0709812aa8b0affd93876931352bc68856fb1f
alin <1714773918@qq.com>
2023-08-13 12:26:59 +0800

    2023-8-13  push test  commit2!

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -6,10 +6,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
+
     @Test
     void testGit() {
         System.out.println("idea third commit!");
-        System.out.println("master new codes");
+        System.out.println("master new codes1");
+        System.out.println("master new codes2");
+        System.out.println("master new codes3");
+        System.out.println("push test!");
+        System.out.println("push test!");
+        System.out.println("push test!");
     }
 
 }
